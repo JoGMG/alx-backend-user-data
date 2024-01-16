245de3f27ded6cccaf728b931042f48406d290f7
JoGMG <joattah3@gmail.com>
2024-01-16 03:28:11 +0100

    0x00. Personal data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python3
 """
-Defines a hash_password function to return a hashed password
+Encrypting and validating passwords for safe storage.
 """
 import bcrypt
-from bcrypt import hashpw
 
 
 def hash_password(password: str) -> bytes:
     """
-    Returns a hashed password
-    Args:
-        password (str): password to be hashed
+    Returns a salted, hashed password.
+
+    Arguments:
+    - `password`: Password to be hashed.
     """
-    b = password.encode()
-    hashed = hashpw(b, bcrypt.gensalt())
+    b_password = password.encode()
+    hashed = bcrypt.hashpw(b_password, bcrypt.gensalt())
     return hashed
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """
-    Check whether a password is valid
-    Args:
-        hashed_password (bytes): hashed password
-        password (str): password in string
-    Return:
-        bool
+    Returns `True` if the provided `password` matches the
+    `hashed password`, else `False`.
+
+    Arguments:
+    - `hashed_password`: Hashed password.
+    - `password`: Normal password.
     """
     return bcrypt.checkpw(password.encode(), hashed_password)
--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Definition of filter_datum function that returns an obfuscated log message
+Obfuscating/filtering Personally Identifiable Information (PII)
 """
 from typing import List
 import re
@@ -15,12 +15,13 @@ PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """
-    Return an obfuscated log message
-    Args:
-        fields (list): list of strings indicating fields to obfuscate
-        redaction (str): what the field will be obfuscated to
-        message (str): the log line to obfuscate
-        separator (str): the character separating the fields
+    Returns an obfuscated log message.
+
+    Arguments:
+    - `fields`: Fields in message to obfuscate
+    - `redaction`: What the field will be obfuscated with
+    - `message`: The log line to obfuscate
+    - `separator`: The character separating the fields in message
     """
     for field in fields:
         message = re.sub(field+'=.*?'+separator,
@@ -29,8 +30,9 @@ def filter_datum(fields: List[str], redaction: str,
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """
+    Redacting Formatter class
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
@@ -42,52 +44,50 @@ class RedactingFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord) -> str:
         """
-        redact the message of LogRecord instance
-        Args:
-        record (logging.LogRecord): LogRecord instance containing message
-        Return:
-            formatted string
+        Filters values in incoming log records using `filter_datum` method
+        and returns filtered message.
+
+        Arguments:
+        - `record`: LogRecord instance containing message.
         """
         message = super(RedactingFormatter, self).format(record)
-        redacted = filter_datum(self.fields, self.REDACTION,
-                                message, self.SEPARATOR)
-        return redacted
+        filtered_message = filter_datum(
+            self.fields, self.REDACTION, message, self.SEPARATOR)
+        return filtered_message
 
 
 def get_logger() -> logging.Logger:
     """
-    Return a logging.Logger object
+    Returns a logging.Logger object.
     """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
 
     handler = logging.StreamHandler()
-
     formatter = RedactingFormatter(PII_FIELDS)
-
     handler.setFormatter(formatter)
+
     logger.addHandler(handler)
     return logger
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """
+    Returns a connector to a database.
     """
-    user = os.getenv('PERSONAL_DATA_DB_USERNAME') or "root"
-    passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ""
-    host = os.getenv('PERSONAL_DATA_DB_HOST') or "localhost"
-    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-    conn = mysql.connector.connect(user=user,
-                                   password=passwd,
-                                   host=host,
-                                   database=db_name)
-    return conn
+    user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    passwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+    connector = mysql.connector.connect(
+        user=user, password=passwd, host=host, database=db_name)
+    return connector
 
 
 def main():
     """
-    main entry point
+    Connects and displays filtered database results.
     """
     db = get_db()
     logger = get_logger()
