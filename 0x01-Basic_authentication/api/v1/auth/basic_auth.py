e42165ba0e40f6290e03b83b00d43a7ee3fbb731
JoGMG <joattah3@gmail.com>
2024-01-17 15:12:47 +0100

    9. Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -46,3 +46,22 @@ class BasicAuth(Auth):
                 base64_authorization_header).decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Returns the user email and password from
+        `decoded_base64_authorization_header`.
+
+        Arguments:
+            - `decoded_base64_authorization_header`: Decoded HTTP
+            request authorization header.
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        credentials = decoded_base64_authorization_header.split(':')
+        return credentials[0], credentials[1]
