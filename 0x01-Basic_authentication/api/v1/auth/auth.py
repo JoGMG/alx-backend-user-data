4bdda40391c361e496193d6a2234e1c5572f37ee
JoGMG <joattah3@gmail.com>
2024-01-17 14:49:25 +0100

    3. Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """
+    API Authentication System Manager.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Returns `True` if `path` requires authentication, else `False`.
+
+        Arguments:
+            - `path`: HTTP request path (url) to check.
+            - `excluded_paths`: List of paths (url) that do not require
+            authentication.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Returns `request` Authorization Header key.
+
+        Arguments:
+            - `request`: HTTP request.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns User instance for a request.
+
+        Arguments:
+            - `request`: HTTP request.
+        """
+        return None
