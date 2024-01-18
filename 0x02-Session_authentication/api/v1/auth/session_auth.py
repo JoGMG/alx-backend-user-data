8acde74e6535ec9e04c0d2b21c80e8521a52f251
JoGMG <joattah3@gmail.com>
2024-01-18 01:29:09 +0100

    1. Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Session Authentication System Management.
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Session Authentication System Manager.
+    """
