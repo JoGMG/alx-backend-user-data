7039ba3c4b798755cf5b8cd93ddb80566701c9d0
JoGMG <joattah3@gmail.com>
2024-01-24 22:02:03 +0100

    6. Basic Flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -9,6 +9,9 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def payload():
+    """
+    Returns a JSON payload.
+    """
     return jsonify({"message": "Bienvenue"})
 
 
