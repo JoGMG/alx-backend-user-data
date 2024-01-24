ccb656f7128fd1c1fc9fb97101ac5d24abf4543a
JoGMG <joattah3@gmail.com>
2024-01-24 22:36:43 +0100

    9. Generate UUIDs

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -19,6 +19,7 @@ def _hash_password(password: str) -> bytes:
     hashed_pword = bcrypt.hashpw(encoded_pword, bcrypt.gensalt())
     return hashed_pword
 
+
 def _generate_uuid() -> str:
     """
     Returns a string representation of a new UUID.
