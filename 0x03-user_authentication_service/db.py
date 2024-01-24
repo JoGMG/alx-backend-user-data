6c34696f4e84546f681eb688261919b6da511b71
JoGMG <joattah3@gmail.com>
2024-01-24 01:30:25 +0100

    1. create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,8 +5,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -35,47 +33,14 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """
-        Creates and saves a user to the database.
-
-        Arguments:
-            - `email`: user's email.
-            - `hash_password`: user's hashed password.
+        Create a User object and save it to the database
+        Args:
+            email (str): user's email address
+            hashed_password (str): password hashed by bcrypt's hashpw
+        Return:
+            Newly created User object
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
-
-    def find_user_by(self, **kwargs) -> User:
-        """
-        Returns the first row found in the `users` table as filtered
-        by the method's input arguments.
-
-        Arguments:
-            - `kwargs`: key-value pairs
-        """
-        all_users = self._session.query(User)
-        for key, value in kwargs.items():
-            if key not in User.__dict__:
-                raise InvalidRequestError
-            for user in all_users:
-                if getattr(user, key) == value:
-                    return user
-        raise NoResultFound
-
-    def update_user(self, user_id: int, **kwargs) -> None:
-        """
-        Updates user attributes using `find_user_by` method
-        to locate user to update.
-
-        Arguments:
-            - `user_id`: User object ID.
-            - `kwargs`: key-value pairs.
-        """
-        user = self.find_user_by(id=user_id)
-        for key, value in kwargs.items():
-            if hasattr(user, key):
-                setattr(user, key, value)
-            else:
-                raise ValueError
-        self._session.commit()
